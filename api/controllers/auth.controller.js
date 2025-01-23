d5f9753cb1d49d348246fa92043b7f881a650086
Piyush-71 <121cs0763@nitrkl.ac.in>
2025-01-23 21:43:27 +0530

    created user models, user and auth routes controllers and handlled error

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.models.js";
+import bycryptjs from "bcryptjs";
+
+export const signup = async (req, res, next) => {
+  const {username, email, password} = req.body;
+  const hasshedPassword = bycryptjs.hashSync(password, 10);
+  const newUser = new User({username, email, password: hasshedPassword});
+  try {
+      await newUser.save();
+      res.status(201).json({message: 'User created successfully'});
+      
+  } catch (error) {
+      next(error);
+  }
+}
--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-mongoose.connect(MONGODB_URI)
+mongoose
+  .connect(MONGODB_URI)
   .then(() => {
     console.log("Connected to MongoDB!!!");
   })
@@ -14,13 +17,22 @@ mongoose.connect(MONGODB_URI)
     console.log(err);
   });
 
-  
-
-
-
-
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
+});
+
+app.use(express.json());
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
