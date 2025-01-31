1fbe498858728bd2a9183d757d342d680073daf8
Piyush-71 <121cs0763@nitrkl.ac.in>
2025-01-31 20:37:52 +0530

    React redux add

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
+
 function SignUp() {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
